ca25dccaabb4109fdcf0136ac1d807b5be404aee
Nick A. Knowles <knowlen@LabImage2016-17>
2017-03-10 18:13:11 -0800

    work in prog

--- sequential_GA.py
+++ sequential_GA.py
@@ -4,14 +4,14 @@
 # approximates a given image using an evolutionary algorithm.
 # NOTES: pip installed futures, pip install numpy, pip install scipy
 
+
 import scipy.misc as sci
 import numpy as np
 import random, time, threading
 import concurrent.futures
-from multiprocessing import Pool
+import multiprocessing
 import argparse
 import os
-from functools import partial
 label_shape = []
 
 def get_parser():
@@ -22,11 +22,11 @@ def get_parser():
     parser = argparse.ArgumentParser("Genetic Algorithm for supervised image approximation.")
     parser.add_argument('image_file', type=str, help='The image file for our supervised training.')
     parser.add_argument('results_folder', type=str, help='The folder to print result images.')
-    parser.add_argument('-epochs', type=int, default=0, help='Number of iterations to train over')
+    parser.add_argument('-epochs', type=int, default=100, help='Number of iterations to train over')
     parser.add_argument('-mutation_prob', nargs='+', type=float, default=[0.15, 0.05],
                         help='chance for a candidate to be randomly mutated.\nCan assign up to 2 unique values here (one for each child produced in crossover).')
-    parser.add_argument("-print_interval",
-                        type=int, default=100, 
+    parser.add_argument("-print_on_epoch",
+                        type=int, default=0, 
 			help="Prints an image every interval of epochs specified.")
     parser.add_argument('-P', type=int, default=500, help='Population size.')
     parser.add_argument('-ds', type=float, default=80,
@@ -57,12 +57,9 @@ class candidate:
     global label_shape
     def __init__(self, init_state=""):
         self.fitness = 0
-###########################       
-# Try this here eventually: 
-#        self.rfit = 0
-#        self.bfit = 0
-#        self.gfit = 0
-###########################
+        self.rfit = 0
+        self.bfit = 0
+        self.gfit = 0
         if init_state == "blank":
             self.img = np.empty(label_shape)
         else:
@@ -86,25 +83,31 @@ class population:
     # look at hr sk sample code. 
         self.pop = [candidate() for i in xrange(size)]
         self.children = []
+        #for champ in champions:
+        #    self.pop.append(champ)
             
-def evaluate(label, sample):
+    # maybe use if need at some point 
+    #def __iter__(self):
+    #    return (i for i in (self.pop, self.children))
+
+
+
+def evaluate(sample, label):
     """
     Evaluates how close a candidate is to the input image label. 
     Updates a candidate's fitness. 
     """
-# numpy is too fast.. Remember to flip back on 
-    #for can in sample: 
-    #error = np.sum(np.absolute(np.subtract(label, sample.img)))
+    for can in sample:
+        #error = np.sum(np.absolute(np.subtract(label, can.img)))
         #can.fitness = 1.0/error
         
-    error = 0;
-    for x,a in zip(sample.img,label):
-        for y,b in zip(x,a):
-            for z,c in zip(y,b):
-                error+= abs(c - z)
-    
-    sample.fitness = 1.0/error
-    return sample
+        error = 0;
+        for x,a in zip(can.img,label):
+            for y,b in zip(x,a):
+                for z,c in zip(y,b):
+                    error+= abs(c - z)
+        
+        can.fitness = 1.0/error
 
 def crossover(p_a, p_b):
     """
@@ -128,7 +131,7 @@ def mutate(child):
     #look into mutation techniques     
     mask = np.random.choice(2, size=label_shape)
     neg_mask = -np.random.choice(2, size=label_shape)
-    scalars = np.random.rand(label_shape[0], label_shape[1], label_shape[2]) #* mask) * neg_mask
+    scalars = np.random.rand(38,38,4) #* mask) * neg_mask
 	#seq element-wise multiplication might be better here
     child.img = child.img + (((child.img*scalars) * mask) * neg_mask)
 
@@ -153,7 +156,6 @@ def tournament_select(pop, t_size, k):
     return selected 
 
 
-# For future steps, incorporate a real replacement alg
 def replacement():
     # truncated: take best N from children & pop
     # elitest: take a few best from pop, rest children
@@ -164,33 +166,18 @@ def replacement():
 if __name__ == '__main__':
     args = get_parser().parse_args()
     label = sci.imread(args.image_file)
+    label = sci.imread('/home/knowlen/Pictures/goog.png')
     label = sci.imresize(label, args.ds)
     label_shape = label.shape
     t_size = 2 #add to argparse later. Should typically be 2 anyways. 
     p = population(args.P) 
     epoch = args.epochs
-    #evaluate(p.pop, label)
+    evaluate(p.pop, label)
     iteration = 0
-    #for i in p.pop:
-    #    print i.fitness
-    img_dir = args.results_folder + '/concurrent__P_' + str(args.P) + '__MaxEpoch_' + str(epoch) 
-    os.system('mkdir ' + img_dir) 
-    interval = args.print_interval 
-    
-# don't print until final image generated 
-    if interval == 0:
-        interval = epoch
-
     while iteration < epoch: 
         
-# Multi processing because Python doesn't support real multi-threading. 
-  	pool = Pool(8) 
-# partial is needed to pass multiple args to a process function. 
-        func = partial(evaluate, label)
-    	evaluated = pool.map(func, p.pop)
-    	pool.close()
-    	pool.join()
-	p.pop = evaluated
+        evaluate(p.pop, label)
+       
         for i in xrange(args.P/2):
             parent_a = tournament_select(p.pop, t_size, 3)
             parent_b = tournament_select(p.pop, t_size, 2)
@@ -205,30 +192,17 @@ if __name__ == '__main__':
             p.children.extend([child_a, child_b])
             if parent_a.fitness < parent_b.fitness:
                 p.pop.remove(parent_a) #change later
-            else: 
+            elif parent_b.fitness < parent_a.fitness:
                 p.pop.remove(parent_b)
         
         p.pop = p.children
-        p.children = [] 
+        p.children = [] # possible memory errors here.
         iteration = iteration + 1
-        #print iteration
-        if iteration % interval == 0:
-            p.pop.sort(key=lambda x: x.fitness, reverse=True)
-            out_fn = img_dir + '/' + str(iteration) + '.png'
-            sci.imsave(out_fn, sci.imresize(p.pop[0].img,100))
-    
-    
-    pool = Pool(8) 
-# partial is needed to pass multiple args to a process function. 
-    func = partial(evaluate, label)
-    evaluated = pool.map(func, p.pop)
-    pool.close()
-    pool.join()
-    p.pop = evaluated
-    p.pop.sort(key=lambda x: x.fitness, reverse=True)
 
+    evaluate(p.pop, label)
+    p.pop.sort(key=lambda x: x.fitness, reverse=True)
 #sci.imshow(sci.imresize(p.pop[0].img, 1000))
-    out_fn = img_dir + '/final_result.png'
+    out_fn = args.results_folder + '/results.png'
     if not os.path.isdir(args.results_folder):
         os.mkdir(args.results_folder) 
     sci.imsave(out_fn, sci.imresize(p.pop[0].img, 100))
